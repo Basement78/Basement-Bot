1667a7145a5673d7bc4bcf68957e49a8229562af
Jamieson Walter <821jjw@gmail.com>
2023-02-01 16:38:31 -0500

    Removed tokens
    Replaced all tokens with descriptions of what to fill them in with.

--- env.js
+++ env.js
@@ -1,11 +1,11 @@
 const tokens = 
 {
-    TOKEN: 'MTA2Nzk0NDEyODAwMTg3NjEwOA.GDp60k.gjeSsMmN_DHp58MmeyM-h0z0tPcZMwmust4IPU',
-    GUILD_ID: '1050189354968031353',
+    TOKEN: 'enter bot token here',
+    GUILD_ID: 'enter guild token here',
     PREFIX: '!',
-    DYNAMICCAT: '1069485287555727400',
-    DYNAMICROOT: '1069813723973292103',
-    STATUS_CHANNEL: '1070145821724639323'
+    DYNAMICCAT: 'ID for category that houses dynamic channels',
+    DYNAMICROOT: 'ID for root channel for dynamic channels',
+    STATUS_CHANNEL: 'ID for channel to post bot logs and up/down status in'
 }
 
 module.exports = tokens
